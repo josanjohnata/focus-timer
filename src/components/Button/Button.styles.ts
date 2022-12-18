834d68a2c335cdd962e9d0abb7a3f80f0c60fe1b
Josan Johnata <josan.pontes@meliuz.com.br>
2022-12-18 11:55:33 -0300

    feat: Theme configurations

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -16,6 +16,12 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
+  border-radius: 4px;
+  border: 0px;
+  margin: 8px;
+
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
 
   /* ${props => {
     return css`
