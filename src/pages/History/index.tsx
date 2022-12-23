d6086efd86863b96674dfeb05ed5ce418e6893ef
Josan Johnata <josan.pontes@meliuz.com.br>
2022-12-22 22:31:20 -0300

    feat: Pages and routes - Style status task

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './style'
+import { HistoryContainer, HistoryList, Status } from './style'
 
 export function History() {
   return (
@@ -20,37 +20,49 @@ export function History() {
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="green">Done</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="yellow">In Progress</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="red">Interrupted</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="red">Interrupted</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="yellow">In Progress</Status>
+              </td>
             </tr>
             <tr>
               <td>Task</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Done</td>
+              <td>
+                <Status statusColor="green">Done</Status>
+              </td>
             </tr>
           </tbody>
         </table>
