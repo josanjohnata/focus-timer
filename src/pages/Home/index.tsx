8316b100c3ab0aee14bd0483eee1c198eaa26b10
Josan Johnata <josan.pontes@meliuz.com.br>
2022-12-25 12:34:30 -0300

    feat: Forms - Reset Form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,7 +24,7 @@ const newCycleFormValidationSchema = zod.object({
 type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<newCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -34,6 +34,7 @@ export function Home() {
 
   const handleCreateNewCycle = (data: newCycleFormData) => {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
